14e3a4bd47c8fee5ccacd770092f25030d599a00
pauloalex01 <passpassjr@gmail.com>
2026-03-04 23:41:43 -0300

    Testes realizados com funções de cadastro, busca e remoção da matrícula

--- interface/cam_service.py
+++ interface/cam_service.py
@@ -1,20 +1,46 @@
-from database.repositories import FerramentaRepository
+import time
+from database.repositories import *
 from interface.decoder_cam import Leitor_codigo
 from database.banco_de_dados import Database
+from database.service import *
+
 
 if __name__ == "__main__":
+    Database.criar_banco()
+    leitor = Leitor_codigo()
+    ferr_service = FerramentaService()
+    mat_service = MatriculaService()
+    fer_repo = FerramentaRepository()
+    mat_repo = MatriculaRepository()
 
-    teste = Leitor_codigo()
-    busca = FerramentaRepository()
-    con = Database.conectar()
+    codigos_lidos = leitor.executar()
 
-    resultado = teste.executar()
+    for item in codigos_lidos:
+        item_str = str(item)
 
-    for item in resultado:
+        try:
+            resultado = ferr_service.cadastrar(item, "Ferramenta")
+            print(f"Código {item_str}: {resultado}")
 
-        ferramenta = busca.buscar_por_codigo(codigo=resultado)
+            time.sleep(0.2)
 
+            fer_info = fer_repo.buscar_por_codigo(item)
+            if fer_info:
+                print(f"Dados no banco: ID:{fer_info[0]} | Status:{fer_info[3]}")
+        except Exception as e:
+            print(f"Erro: {item_str}: {e}")
 
+    matricula = input("Digite sua matricula: ")
+    matricula_resultado = mat_service.remover(matricula)
+    print(f"Matricula: {matricula}")
 
+    time.sleep(0.1)
 
+    mat_info = mat_repo.buscar_por_matricula(matricula)
 
+    if mat_info:
+        print(f"ID: {mat_info[0]}")
+        print(f"Matrícula: {mat_info[1]}")
+        print(f"Nome: {mat_info[2]}")
+    else:
+        print("Erro: Matrícula não encontrada no banco de dados.")
