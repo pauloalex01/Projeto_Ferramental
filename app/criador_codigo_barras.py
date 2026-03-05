c1a413639754cbfde9e729776e039c2f6c1bf943
pauloalex01 <passpassjr@gmail.com>
2026-03-04 23:45:40 -0300

    Adicionando o arquivo .sqlite com o database que estaremos a usar

---
+++ app/criador_codigo_barras.py
@@ -0,0 +1,49 @@
+import barcode
+from barcode.writer import ImageWriter
+import shutil
+import os
+
+
+def criar_codigo_barras(codigo):
+
+    if len(str(codigo)) != 12:
+        return "Código deve conter exatamente 12 dígitos numéricos"
+
+    # Diretório base do projeto
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+
+    pasta_destino = os.path.join(base_dir, "barcodes")
+    os.makedirs(pasta_destino, exist_ok=True)
+
+    # Criar código de barras
+    codigo_de_barras = barcode.get('ean13', codigo, writer=ImageWriter())
+    caminho_gerado = codigo_de_barras.save(os.path.join(base_dir, codigo))
+
+    # Caminho final
+    nome_arquivo = os.path.basename(caminho_gerado)
+    caminho_final = os.path.join(pasta_destino, nome_arquivo)
+
+    # Mover arquivo
+    shutil.move(caminho_gerado, caminho_final)
+
+    return print(f'Código de barras salvo em: {caminho_final}')
+
+
+teste = [
+"789123450001",
+"789123450002",
+'789123450003',
+"789123450004",
+"789123450005",
+"789123450006",
+"789123450007",
+"789123450008",
+"789123450009",
+"789123450010",
+"789123450011",
+"789123450012"
+]
+
+
+for i in teste:
+    criar_codigo_barras(i)
